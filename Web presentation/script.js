09d36d482437077c50c8c89463b84f3746c8cc1f
Khateeb Ur Rehman <121893141+khateeburrehman66@users.noreply.github.com>
2023-06-16 19:41:23 +0500

    Add files via upload

---
+++ Web presentation/script.js
@@ -0,0 +1,7 @@
+//applying transition text side 1
+// window.addEventListener('load', function()
+// {
+//     var slide1text=document.querySelector('.slide1div2trans');
+//     slide1text.classList.add('slidetext');
+// });
+
